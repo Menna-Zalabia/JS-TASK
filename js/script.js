ef89e7d159d54924f6cba0e803ffbc2c0235a506
Menna-Zalabia <mennazalabia68@gmail.com>
2022-11-24 12:20:26 +0200

    change

--- js/script.js
+++ js/script.js
@@ -1,22 +1,29 @@
 var btn = document.querySelectorAll(".item #addToCart")
 var content = document.querySelector(".content")
 var allproduct = document.querySelector(".pro")
-var btn1=document.querySelector("#btn1")
-var totalPrice = 0 ;
-var products="";
+var btn1 = document.querySelector("#btn1")
+var btn2 = document.querySelector("#btn2")
+var totalPrice = 0;
+var products = "";
 
-btn.forEach( function (item){
-    item.onclick = function (){
-        products= item.getAttribute("name")
-        allproduct.innerHTML += products+"<br>";
+btn.forEach(function (item) {
+    item.onclick = function () {
+        products = item.getAttribute("name")
+        allproduct.innerHTML += products + "<br>";
         totalPrice += parseFloat(item.getAttribute("price"))
-        content.innerHTML ="The Price "+"= "+totalPrice + " EGP ";
-        if(content.innerHTML !=0){
-            btn1.style.visibility="visible";
+        if (allproduct.innerHTML != 0) {
+            btn2.style.visibility = "visible"
         }
+        btn2.onclick = function () {
+            content.innerHTML = "The Price " + "= " + totalPrice + " EGP ";
+            if (content.innerHTML != 0) {
+                btn1.style.visibility = "visible";
+            }
+        }
+       
     }
 })
-btn1.onclick=function(){
+btn1.onclick = function () {
     alert("The Payment completed successfully :)")
 }
 
